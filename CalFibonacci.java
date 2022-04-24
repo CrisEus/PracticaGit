934aa2b45b0a63925066dafc89a736345a3045bc
CrisEus <cbaezfernandezc@gmail.com>
2022-04-24 22:46:18 +0200

    Comenzando la practica

---
+++ CalFibonacci.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fibonacci;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eustral
+ */
+public class CalFibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic 
+
+        Scanner miScan = new Scanner(System.in);
+        String sSalir;
+        
+        int iValor = 0;
+        String sResultado = "";
+        String sAux;
+//Nodo1
+
+        do {
+            System.out.println("Pulsa intro comenzar o S, s para salir..");
+            sSalir = miScan.nextLine();
+            System.out.println("¿Cuantos números de la serie desea demostrar?");
+            sAux = miScan.nextLine();
+            iValor = Integer.parseInt(sAux);
+            switch (iValor) {
+                case 3:
+                    sResultado = "1";
+                    break;
+                case 2:
+                    sResultado = "0" + sResultado;
+                    break;
+                case 1:
+                    sResultado = "0" + sResultado;
+                    break;
+            }
+        } while (!(sSalir.equals("S") || sSalir.equals("S")));
+        
+        sSalir = miScan.nextLine();
+    }
+}
